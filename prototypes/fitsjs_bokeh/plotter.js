d3adc4ce3a407128dbb0441e5662a008839d890f
Denys SAVCHENKO <speleoden@gmail.com>
2024-02-20 18:04:26 +0100

    improve lightcurve

--- prototypes/fitsjs_bokeh/plotter.js
+++ prototypes/fitsjs_bokeh/plotter.js
@@ -5,25 +5,43 @@ const showPlot = function() {
     const plot_lc = function(x, y, dy, timedel) {
         const plt = Bokeh.Plotting;
 
-        let ylow = [], yup = [], xlow = [], xup = [];
+        // bin width calculation is from oda_api, simplified, e.g. timepix = 0.5 always
+        let t_lc = x;
+        let dt_lc = new Array(x.length).fill(timedel / 2);
+        for (let i=0; i < dt_lc.length - 1; i++) {
+            dt_lc[i+1] = Math.abs(Math.min(timedel / 2, t_lc[i+1] - t_lc[i] - dt_lc[i]))
+        }
+
+        let yer_xs = [], yer_ys = [], xer_xs = [], xer_ys = [];
+        let ylow, yup; 
         for (let i in dy) {
-            ylow[i] = y[i] - dy[i];
-            yup[i] = y[i] + dy[i];
-            xlow[i] = x[i] - timedel / 2;
-            xup[i] = x[i] + timedel / 2; // bin width calculation needs to be improved
+            ylow = ylow ? Math.min(ylow, y[i] - dy[i]) : y[i] - dy[i];
+            yup = yup ? Math.max(yup, y[i] + dy[i]): y[i] + dy[i];
+            yer_xs[i] = [x[i], x[i]];
+            yer_ys[i] = [y[i] - dy[i], y[i] + dy[i]];
+            xer_xs[i] = [x[i] - dt_lc[i], x[i] + dt_lc[i]];
+            xer_ys[i] = [y[i], y[i]];
         }
 
         const source = new Bokeh.ColumnDataSource({
-            data: { x: x, y: y, ylow: ylow, yup: yup, xlow: xlow, xup: xup }
+            data: {
+                x: x, 
+                y: y, 
+                dy: dy, 
+                yer_xs: yer_xs, 
+                yer_ys: yer_ys,
+                xer_xs: xer_xs,
+                xer_ys: xer_ys
+            }
         });
-        
-        const tools = "pan,box_zoom,wheel_zoom,hover,crosshair,reset,save";
+
+        const tools = "pan,box_zoom,box_select,wheel_zoom,hover,crosshair,reset,save";
         const p = plt.figure({ 
             title: "Lightcurve", 
             tools: tools,
             width: 800,
-            height: 600,
-            y_range: [Math.min(...ylow), Math.max(...yup)],
+            height: 500,
+            y_range: [ylow, yup],
         });
 
         p.xaxis.axis_label = 'TIME';
@@ -35,33 +53,38 @@ const showPlot = function() {
             fill_color: "navy",
             line_color: null,
         });
+        circles.selection_glyph = new Bokeh.Circle({fill_color: "red", line_color: null})
+        circles.nonselection_glyph = new Bokeh.Circle({fill_color: "navy", line_color: null})
 
-        const yerr_bar = new Bokeh.Whisker({
-            dimension: "height",
-            source: source,
-            base: {field: "x"},
-            lower: {field: "ylow"},
-            upper: {field: "yup"},
+        const yerr_bar = new Bokeh.MultiLine({
+            xs: {field: 'yer_xs'},
+            ys: {field: 'yer_ys'},
             line_color: "navy",
-            lower_head: null,
-            upper_head: null,
         });
-        p.add_layout(yerr_bar);
+        const yb = p.add_glyph(yerr_bar, source);
+        yb.selection_glyph = new Bokeh.MultiLine({line_color: 'red'});
 
-        const xerr_bar = new Bokeh.Whisker({
-            dimension: "width",
-            source: source,
-            base: {field: "y"},
-            lower: {field: "xlow"},
-            upper: {field: "xup"},
+        const xerr_bar = new Bokeh.MultiLine({
+            xs: {field: 'xer_xs'},
+            ys: {field: 'xer_ys'},
             line_color: "navy",
-            lower_head: null,
-            upper_head: null,
         });
-        p.add_layout(xerr_bar);
+        const xb = p.add_glyph(xerr_bar, source);
+        xb.selection_glyph = new Bokeh.MultiLine({line_color: 'red'});
+
+        const columns = [
+            new Bokeh.Tables.TableColumn({field: 'x', title: 'TIME'}),
+            new Bokeh.Tables.TableColumn({field: 'y', title: 'RATE'}),
+            new Bokeh.Tables.TableColumn({field: 'dy', title: 'ERROR'}),
+        ]
+        const data_table = new Bokeh.Tables.DataTable({
+            source: source,
+            columns: columns,
+            width: 800,
+        });
 
         // show the plot
-        plt.show(p, "#container");
+        plt.show(new Bokeh.Column({children: [p, data_table]}), "#plot_container");
     }
 
     const callback = function() {
@@ -69,8 +92,8 @@ const showPlot = function() {
         let header = hdu.header;
         let data = hdu.data;
         
-        let timedel = header.get('TIMEDEL'); 
-
+        let timedel = header.get('TIMEDEL');
+        
         let x, y, dy;
         data.getColumn("TIME", function(col){x = col});
         data.getColumn("RATE", function(col){y = col});
